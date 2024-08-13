6ea316069322dd3f6543f0b12ce94f04d2b66afa
diego-04rojas <est.diego.arodriguez@unimilitar.edu.co>
2024-08-13 15:45:00 -0500

    Calculadora

---
+++ src/main/java/com/mycompany/micalculadora/MiCalculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.micalculadora;
+
+/**
+ *
+ * @author Multipropósito2
+ */
+public class MiCalculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
